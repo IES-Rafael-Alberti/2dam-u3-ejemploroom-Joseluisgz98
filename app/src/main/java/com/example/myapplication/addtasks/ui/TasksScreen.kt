813db34f4dedd3278a1dc178bcbd0abf496ed33c
joseluisgz98 <joseluis_cadiz98@hotmail.com>
2024-01-12 19:40:08 +0100

    primera parte casi terminada

---
+++ app/src/main/java/com/example/myapplication/addtasks/ui/TasksScreen.kt
@@ -0,0 +1,106 @@
+package com.example.myapplication.addtasks.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun TasksScreen(viewModel: TasksViewModel){
+    val showDialog: Boolean by viewModel.showDialog.observeAsState(false)
+    val myTaskText: String by viewModel.myTaskText.observeAsState("")
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(
+            show = showDialog,
+            myTaskText = myTaskText,
+            onDismiss = { viewModel.onDialogClose() },
+            onTaskAdded = {viewModel.onTaskCreated() },
+            onTaskTextChanged = { viewModel.onTaskTextChanged(it) }
+        )
+        FabDialog(
+            Modifier.align(Alignment.BottomEnd),
+            onNewTask = { viewModel.onShowDialogClick() })
+        //TasksList(viewModel)
+    }
+
+
+}
+@Composable
+fun FabDialog(
+    modifier: Modifier,
+    onNewTask: () -> Unit
+) {
+    FloatingActionButton(
+        onClick = {
+            onNewTask()
+        }, modifier = modifier.padding(16.dp)
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTasksDialog(
+    show: Boolean,
+    myTaskText: String,
+    onDismiss: () -> Unit,
+    onTaskAdded: () -> Unit,
+    onTaskTextChanged: (String) -> Unit
+) {
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Añade tu tarea",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTaskText,
+                    onValueChange = { onTaskTextChanged(it) },
+                    singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(
+                    onClick = {
+                        onTaskAdded()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Añadir tarea")
+                }
+            }
+        }
+    }
+}
